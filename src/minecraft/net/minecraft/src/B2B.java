91df5dfbd60a1320a7aac9ad2c0f895b823537df
Luna Chrysalis <nawathoare@outlook.com>
2016-04-30 00:26:11 -0400

    Beta 1.8.3_r54.1
    First Build of r54, r54.1 to r54.10 is to regain stability of B2B ever
    since Luna's MacBook Pro screwed up the stability.
    
    Things to rebuild
    - Textures are Damaged, Will send sprites via new branch
    - LWJGL likes to crash
    - Fix for Retina MacBooks and iMacs
    - Apply Armor Meshes for Ardum, ETC
    - Add Lapis Tools
    - Tool Recipie for Osmium Rod
    - Crafting recipie for Ardum (Ardum Ingots + Osmium Rods)
    - Apply Pyrite Ingot
    - HTC One M8 Item only for Debug (Only works for dopplgngr and LunaRay70
    players)
    
    + Dopplgngr has rejoined to help stabilize B2B

--- src/minecraft/net/minecraft/src/B2B.java
+++ src/minecraft/net/minecraft/src/B2B.java
@@ -3,7 +3,7 @@ package net.minecraft.src;
 
 public class B2B {
 // this code does nothing except set the version string	
-	public static String version = "Minecraft Beta 1.8.3_r52";
+	public static String version = "Minecraft Beta 1.8.3_r54";
 }
 // c2 will be r55
 // final release will be r60 or c3
